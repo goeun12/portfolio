88bdca67c52441d5e5493245359fd58f6decbe0c
goeun12 <reanpb@gmail.com>
2024-08-22 13:04:55 +0900

    Add files via upload

---
+++ moblie_identityCard/src/main/java/com/spring/data/SpringData1Application.java
@@ -0,0 +1,13 @@
+package com.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringData1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringData1Application.class, args);
+	}
+
+}
